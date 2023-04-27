37954673e121feeed947d6f0bc0f566436a1227d
oliahrytsak <oliagrytsak1991@gmail.com>
2023-04-27 14:24:59 +0300

    js

--- js/header.js
+++ js/header.js
@@ -1,3 +1,11 @@
-document.addEventListener("partialsLoaded", () => {
-    import('./header.js');
-})
+(function () {
+
+    const clockContainer = document.querySelector('.header___clock');
+
+    function updateTime() {
+        clockContainer.innerText = new Date().toLocaleTimeString();
+    }
+
+    setInterval(updateTime, 1000);
+
+})();
