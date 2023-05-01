66a898c1ec0863ee0c0418c9db5bb5979451a415
Zickfried <123055065+Zickfried@users.noreply.github.com>
2023-05-01 12:53:23 +0000

    check mistake into js into faq

--- js/faq.js
+++ js/faq.js
@@ -1,6 +1,6 @@
 const acordion = document.querySelectorAll(".acordion__part");
 
-for (i=0; i<acordion.length; i++) {
+for (let i=0; i<acordion.length; i++) {
     acordion[i].addEventListener('click', function() {
       this.classList.toggle('active')
     })
