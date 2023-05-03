c99f84e0120c0620c8c0cf81ff038bdc43b54643
Zickfried <123055065+Zickfried@users.noreply.github.com>
2023-05-03 18:19:48 +0000

    add in js faq renove active after double click in sign of acordion

--- js/faq.js
+++ js/faq.js
@@ -3,8 +3,10 @@ const acordion = document.querySelectorAll(".acordion__part");
 for (let i=0; i<acordion.length; i++) {
     acordion[i].addEventListener('click', function() {
       for (let j = 0; j < acordion.length; j++) {
-        acordion[j].classList.remove('active');
-    }
-    this.classList.add('active');
-  });
-}
+        if (acordion[j].classList.contains("active") && acordion[j] !== this) {
+          acordion[j].classList.remove("active");
+        }
+      }
+      this.classList.toggle("active");
+    });
+  }
