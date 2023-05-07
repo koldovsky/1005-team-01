67c15ae308fbdac741a64c926d62fe36951a113a
mojoym <mojoym@gmail.com>
2023-05-07 10:48:17 +0300

    Places block json added

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ document.addEventListener("partialsLoaded", () => {
     import('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js');
     import('./header-clock.js');
     import('./burger-menu.js');
+    import('./places.js');
     import('./tour.js');
     import('./address.js');
     import('./header-phrase.js');
