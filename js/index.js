21da75b5513abb24e9bcbffaa4f0c5544abf7c13
mojoym <mojoym@gmail.com>
2023-05-07 21:31:01 +0300

    add-places.js cpmmented in index.js

--- js/index.js
+++ js/index.js
@@ -2,8 +2,7 @@ document.addEventListener("partialsLoaded", () => {
     import('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js');
     import('./header-clock.js');
     import('./burger-menu.js');
-    //import('./places.js');
-    import('./add-places.js');
+//    import('./add-places.js');
     import('./places-menu.js');
     import('./tour.js');
     import('./address.js');
